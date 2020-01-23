f133fe20a35900d6520c3b80877e9cbec1def604
Jitesh <jitesh.khatwani@axelerant.com>
2020-01-23 14:31:25 +0530

    Issue #12586 - Added content on search page with all the details which is required.

--- profile/modules/apps/os_app_global/src/Controller/AppGlobalContentController.php
+++ profile/modules/apps/os_app_global/src/Controller/AppGlobalContentController.php
@@ -8,6 +8,13 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Drupal\vsite\Plugin\AppManager;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\search_api\Entity\Index;
+use Drupal\Core\Access\AccessResult;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api_page\SearchApiPageInterface;
+use Drupal\search_api\Query\ResultSetInterface;
 
 /**
  * Controller for the cp_users page.
@@ -38,13 +45,21 @@ class AppGlobalContentController extends ControllerBase {
   protected $entityManager;
 
   /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
    * Class constructor.
    */
-  public function __construct(RequestStack $request_stack, AppManager $app_manager, EntityTypeManagerInterface $entity_manager) {
+  public function __construct(RequestStack $request_stack, AppManager $app_manager, EntityTypeManagerInterface $entity_manager, RendererInterface $renderer) {
     $this->requestStack = $request_stack;
     $this->appManager = $app_manager;
     $this->entityManager = $entity_manager;
     $this->nodeStorage = $this->entityManager->getStorage('node');
+    $this->renderer = $renderer;
   }
 
   /**
@@ -54,80 +69,204 @@ class AppGlobalContentController extends ControllerBase {
     return new static(
       $container->get('request_stack'),
       $container->get('vsite.app.manager'),
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('renderer')
     );
   }
 
   /**
    * Check if request app url is available or not.
    */
-  public function getAppList() {
+  public function getAppList(Request $request) {
+
+    $build = [];
 
     $app_requested = $this->requestStack->getCurrentRequest()->attributes->get('app');
 
-    $available_apps = $this->getAvailableApps($app_requested);
+    $available_bundle_content = $this->loadApp($app_requested);
 
-    if (in_array($app_requested, $available_apps['app_id'])) {
-      $available_bundle_content = $this->loadApp($available_apps['entity_type'], $app_requested);
-      ksm($available_bundle_content);
-      return $this->redirect('<front>', [], ['absolute' => TRUE]);
-    }
+    $search_api_page_name = 'search';
+    $build['#theme'] = 'search_api_page';
+    /* @var $search_api_page \Drupal\search_api_page\SearchApiPageInterface */
+    $search_api_page = $this->entityManager()
+      ->getStorage('search_api_page')
+      ->load($search_api_page_name);
 
-    return $this->redirect('<front>', [], ['absolute' => TRUE]);
-  }
+    $result = $available_bundle_content;
 
-  /**
-   * List of Available apps, with entity type.
-   */
-  private function getAvailableApps($app_requested) {
-    $available_apps = [];
-    $app_details = [];
-    $app_entity_type = '';
+    /* @var $items \Drupal\search_api\Item\ItemInterface[] */
+    $items = $result->getResultItems();
 
-    /** @var \Drupal\vsite\AppInterface[] $apps */
-    $apps = $this->appManager->getDefinitions();
-
-    foreach ($apps as $key => $app) {
-      $available_apps[$key] = $app['id'];
-      if ($app_requested == $app['id']) {
-        $app_entity_type = $app['entityType'];
+    $results = [];
+    foreach ($items as $item) {
+      $rendered = $this->createItemRenderArray($item, $search_api_page);
+      if ($rendered === []) {
+        continue;
       }
+      $results[] = $rendered;
     }
-    $app_details['app_id'] = $available_apps;
-    $app_details['entity_type'] = $app_entity_type;
-    return $app_details;
+
+    return $this->finishBuildWithResults($build, $result, $results, $search_api_page);
   }
 
   /**
    * Load index based on requested app.
    */
-  private function loadApp($app_entity_type, $app_requested) {
+  private function loadApp($app_requested) {
+
+    /** @var \Drupal\vsite\AppInterface[] $apps */
+    $enabled_apps = $this->appManager->getDefinitions();
+
     $index = Index::load('os_search_index');
     $query = $index->query();
     $query->keys('');
-    $query->setOption('custom_search_bundle', [
-      'custom_search_bundle' => [
-        'field' => 'custom_search_bundle',
-        'limit' => 90,
-        'operator' => 'OR',
-        'min_count' => 1,
-        'missing' => FALSE,
-      ],
-    ]);
-    $query->sort('search_api_relevance', 'ASC');
+    $enabled_apps_list = [];
+
+    if (isset($enabled_apps[$app_requested]['bundle'])) {
+      $enabled_apps_list = array_merge($enabled_apps_list, $enabled_apps[$app_requested]['bundle']);
+    }
+    else {
+      $enabled_apps_list[] = $enabled_apps[$app_requested]['entityType'];
+    }
+
+    if ($enabled_apps_list) {
+      $query->addCondition('custom_search_bundle', $enabled_apps_list, 'IN');
+    }
+
     $results = $query->execute();
-    $facets = $results->getExtraData('elasticsearch_response', []);
-    $facets_result = $facets['hits']['hits'];
-    foreach ($facets_result as $key => $facet) {
-      if ($facet['_source']['custom_search_bundle'][0] == $app_requested) {
-        $facet_content['title'][$key] = $facet['_source']['custom_title'];
-        $facet_content['body'][$key] = '';
-        if ($facet['_source']['body'] !== NULL && isset($facet['_source']['body'])) {
-          $facet_content['body'][$key] = $facet['_source']['body'];
-        }
+
+    return $results;
+  }
+
+  /**
+   * Checks access for page /browse/{app}.
+   */
+  public function access() {
+    $app_requested = $this->requestStack->getCurrentRequest()->attributes->get('app');
+
+    /** @var \Drupal\vsite\AppInterface[] $apps */
+    $apps = $this->appManager->getDefinitions();
+    foreach ($apps as $app) {
+      if ($app['id'] == $app_requested) {
+        return AccessResult::allowed();
       }
     }
-    return $facet_content;
+
+    // Return 403 Access Denied page.
+    return AccessResult::forbidden();
+  }
+
+  /**
+   * Creates a render array for the given result item.
+   *
+   * @param \Drupal\search_api\Item\ItemInterface $item
+   *   The item to render.
+   * @param \Drupal\search_api_page\SearchApiPageInterface $search_api_page
+   *   The search api page.
+   *
+   * @return array
+   *   A render array for the given result item.
+   */
+  protected function createItemRenderArray(ItemInterface $item, SearchApiPageInterface $search_api_page) {
+    try {
+      $originalObject = $item->getOriginalObject();
+      if ($originalObject === NULL) {
+        return [];
+      }
+      /** @var \Drupal\Core\Entity\EntityInterface $entity */
+      $entity = $originalObject->getValue();
+    }
+    catch (SearchApiException $e) {
+      return [];
+    }
+
+    if (!$entity) {
+      return [];
+    }
+
+    $viewedResult = [];
+    if ($search_api_page->renderAsViewModes()) {
+      $datasource_id = 'entity:' . $entity->getEntityTypeId();
+      $bundle = $entity->bundle();
+      $viewMode = $search_api_page->getViewModeConfig()->getViewMode($datasource_id, $bundle);
+      $viewedResult = $this->entityTypeManager()->getViewBuilder($entity->getEntityTypeId())->view($entity, $viewMode);
+    }
+    if ($search_api_page->renderAsSnippets()) {
+      $viewedResult = [
+        '#theme' => 'search_api_page_result',
+        '#item' => $item,
+        '#entity' => $entity,
+      ];
+    }
+
+    $metadata = CacheableMetadata::createFromRenderArray($viewedResult);
+    $metadata->addCacheTags(['search_api_page.style']);
+    $metadata->applyTo($viewedResult);
+    return $viewedResult;
+  }
+
+  /**
+   * Adds the results to the given build and then finishes it.
+   *
+   * @param array $build
+   *   The build.
+   * @param \Drupal\search_api\Query\ResultSetInterface $result
+   *   Search API result.
+   * @param array $results
+   *   The result item render arrays.
+   * @param \Drupal\search_api_page\SearchApiPageInterface $search_api_page
+   *   The search api page.
+   *
+   * @return array
+   *   The finished build.
+   */
+  protected function finishBuildWithResults(array $build, ResultSetInterface $result, array $results, SearchApiPageInterface $search_api_page) {
+
+    $build['#search_title'] = [
+      '#markup' => $this->t('Search results'),
+    ];
+
+    $build['#no_of_results'] = [
+      '#markup' => $this->formatPlural($result->getResultCount(), '1 result found', '@count results found'),
+    ];
+
+    $build['#results'] = $results;
+
+    pager_default_initialize($result->getResultCount(), $search_api_page->getLimit());
+    $build['#pager'] = [
+      '#type' => 'pager',
+    ];
+
+    return $this->finishBuild($build, $search_api_page, $result);
+  }
+
+  /**
+   * Finishes the build.
+   *
+   * @param array $build
+   *   An array containing all page elements.
+   * @param \Drupal\search_api_page\SearchApiPageInterface $searchApiPage
+   *   The Search API page entity.
+   * @param \Drupal\search_api\Query\ResultSetInterface $result
+   *   Search API result.
+   *
+   * @return array
+   *   An array containing all page elements.
+   */
+  protected function finishBuild(array $build, SearchApiPageInterface $searchApiPage, ResultSetInterface $result = NULL) {
+    $this->moduleHandler()->alter('search_api_page', $build, $result, $searchApiPage);
+
+    // TODO caching dependencies.
+    // @see https://www.drupal.org/project/search_api_page/issues/2754411.
+    return $build;
+  }
+
+  /**
+   * Title callback.
+   */
+  public function getTitle() {
+    // Provide a dynamic title.
+    return ucfirst($this->requestStack->getCurrentRequest()->attributes->get('app'));
   }
 
 }
