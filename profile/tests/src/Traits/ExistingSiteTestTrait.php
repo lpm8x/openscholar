fd57158a84503993e65c5c6d9e962ff6959fdc81
Balázs Sztreha <balazs.sztreha@cheppers.com>
2020-02-07 15:19:59 +0100

    Issue #12747 - Move createRedirect function and remove Base class

--- profile/tests/src/Traits/ExistingSiteTestTrait.php
+++ profile/tests/src/Traits/ExistingSiteTestTrait.php
@@ -387,6 +387,27 @@ trait ExistingSiteTestTrait {
   }
 
   /**
+   * Creates a redirect.
+   *
+   * @param array $values
+   *   (optional) The values used to create the entity.
+   *
+   * @return \Drupal\redirect\Entity\Redirect
+   *   The created redirect entity.
+   */
+  protected function createRedirect(array $values = []) : EntityInterface {
+    $redirect = $this->container->get('entity_type.manager')->getStorage('redirect')->create($values + [
+      'type' => 'redirect',
+    ]);
+    $redirect->enforceIsNew();
+    $redirect->save();
+
+    $this->markEntityForCleanup($redirect);
+
+    return $redirect;
+  }
+
+  /**
    * Place block content to region.
    *
    * @param \Drupal\block_content\BlockContentInterface $block_content
